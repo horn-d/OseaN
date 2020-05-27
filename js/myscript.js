90d7c73099f0f99b1daa87c25208a8f716684890
horn-d <56089647+horn-d@users.noreply.github.com>
2020-05-27 09:11:02 +0200

    version 8 270520

--- js/myscript.js
+++ js/myscript.js
@@ -4,6 +4,9 @@
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiYmVuZWRpY3Rob3JuIiwiYSI6ImNrYWh0c282djBqbHQzM280OWcxNTBvM2MifQ.X1GAcQy2BFLpIb_lbUP2hg';
 
+
+
+
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
@@ -77,3 +80,65 @@ geojson.features.forEach(function (marker) {
         .addTo(map);
 
 });
+
+
+
+
+
+
+function myFunction () {
+    alert ("I am an alert box!")
+}
+
+
+
+
+
+
+
+
+"use strict";
+
+function dialog(hoejde, bredde, baggrundsfarve, fontfarve, placering, indhold, knapper) {
+
+    const htmlkrop = document.body;
+    htmlkrop.insertAdjacentHTML('afterend', '<div id="modaldialog" class="modal_dialog"><div id="modalindhold"></div></div>');
+    // afterbody er efter </body> hvor der bliver indsat en <div id="modaldialog" classs="modaldialog">
+
+    const modalindhold = document.getElementById("modalindhold");
+    modalindhold.classList.add("modal_indhold");
+
+    const modaldialog = document.getElementById("modaldialog");
+    let dialogIndhold = '<span onclick="modaldialog.remove()" class="close">X</span><p><br>' + indhold + '</p>';
+    // "modaldialog.remove() fjerner modaldialog boksen ved tryk på kryds
+
+    modalindhold.style.width = bredde;
+    modalindhold.style.height = hoejde;
+    modalindhold.style.backgroundColor = baggrundsfarve;
+    modalindhold.style.color = fontfarve;
+    //
+
+    if (placering === "centrer") {
+        modalindhold.style.left = "50%";
+        modalindhold.style.top = "50%";
+        modalindhold.style.transform = "translate(-50%,-50%)";
+        modalindhold.style.boxShadow = "5px 5px 5px black";
+    } else if (placering === "bund") {
+        modalindhold.style.left = "0";
+        modalindhold.style.bottom = "0";
+    } else if (placering === "top") {
+        modalindhold.style.left = "0";
+        modalindhold.style.top = "0";
+
+    }if (knapper) {
+        dialogIndhold += '<div class="knappanel"><button type="button" class="knapper">Fortryd</button><button type="button" class="knapper">OK</button></div>';}modalindhold.innerHTML = dialogIndhold;}
+
+// Hovedprogram
+document.getElementById("topknap").addEventListener("click", function () {
+    dialog("150px", "100%", "lightgrey", "red", "top", "Toppen", true);});
+
+document.getElementById("bundknap").addEventListener("click", function () {
+    dialog("150px", "100%", "blue", "red", "bund", "Bunden", true);});
+
+document.getElementById("centrerknap").addEventListener("click", function () {
+    dialog("150px", "350px", "yellow", "blue", "centrer", "Centreret", false);});
